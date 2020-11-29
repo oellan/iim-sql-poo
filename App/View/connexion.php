c9da5e7caea0926afce6e8c71f372a884d285da4
Sofian <sofian.slimani@edu.devinci.fr>
2020-11-29 17:51:59 +0100

    :construction: page connexion

---
+++ App/View/connexion.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Connexion</title>
+</head>
+<body>
+<h1>Connectez-vous</h1>
+
+<form>
+    <div>
+
+        <input type="text" id="name" name="user_name"  placeholder="Pseudo" />
+
+                <input type="email" id="mail" name="user_email" />
+
+            
+                <label for="msg">Message:</label>
+                <textarea id="msg" name="user_message"></textarea>
+
+</form>
+</body>
+</html>
