626e6f527789c39e3cbe773508b2f3abe4ccaa2a
Sofian <sofian.slimani@edu.devinci.fr>
2020-11-30 15:42:37 +0100

    ajout page sondage

--- router.php
+++ router.php
@@ -4,18 +4,22 @@ use App\Controller\HomeController;
 use App\Controller\LoginController;
 use App\Controller\RegisterController;
 
-if(array_key_exists("page", $_GET)){
-    switch ($_GET["page"]) {
-        case 'login':
-            (new LoginController())->renderIndex();
-            break;
-        case 'register':
-            (new RegisterController())->renderIndex();
-            break;
-        default:
-            # code...
-            break;
-    }
+if(array_key_exists("page", $_GET)) switch ($_GET["page"]) {
+    case 'login':
+        (new LoginController())->renderIndex();
+        break;
+    case 'register':
+        (new RegisterController())->renderIndex();
+        break;
+
+    case 'sondage':
+        (new SondageController())->renderIndex();
+        break;
+
+
+    default:
+        # code...
+        break;
 } else{
     $controller = new HomeController();
     $controller->renderIndex();
