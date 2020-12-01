c7abcdf456b3c1f6cfc58b803d071703d81876f0
Sofian <sofian.slimani@edu.devinci.fr>
2020-12-01 20:13:02 +0100

    :construction: home

--- App/View/homeView.php
+++ App/View/homeView.php
@@ -12,9 +12,21 @@
  ?>
 
 <main>
-    <h1>Bienvenue sondeur !</h1>
-    <section class="sondages_amis"
-        <p>Sondages de vos amis</p>
+    <div class="top_main">
+        <h1>Bienvenue sondeur !</h1>
+    </div>
+
+    <section class="sondages_amis">
+
+        <p class="titre_sondage_amis">Sondages de vos amis</p>
+
+    </section>
+    <section class="sondages_passer">
+
+        <p class="titre_sondage_passer">Sondages récents</p>
+
+    </section>
+
 
 </main>
 
