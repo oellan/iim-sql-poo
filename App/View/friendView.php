bec4b374ae52b1d9878c77e982760513e8e5d097
Sofian <sofian.slimani@edu.devinci.fr>
2020-12-01 16:43:01 +0100

    fix friend page

--- App/View/friendView.php
+++ App/View/friendView.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inscription</title>
-    <link rel="stylesheet" href="css/friends?t=<?php echo time(); ?>.css">
+    <title>Recherche d'amis</title>
+    <link rel="stylesheet" href="css/friends.css">
 
 </head>
 <body>
