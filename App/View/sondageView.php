7a93efa3ab1f35b0ef721be0f8c5074201cc932c
Sofian <sofian.slimani@edu.devinci.fr>
2020-12-01 17:58:41 +0100

    fix css

--- App/View/sondageView.php
+++ App/View/sondageView.php
@@ -19,23 +19,15 @@ include ("include/header.php");
 <div class="contour">
 
     <form class="form" method="post">
-        <p id="question">Qui a inventé les transports en communs modernes ?
+        <p id="question">Que manger ce soir ?
         </p>
         <div class="line">
-            <label for="response_1">Séraphin Lampion</label>
+            <label for="response_1">Des pâtes</label>
             <input type="radio" class="radio" name="response" value="response_1"  id="response_1"/>
         </div>
         <div class="line">
-            <label for="response_2">Blaise Pascal</label>
+            <label for="response_2">Pizza</label>
             <input type="radio" class="radio" name="response" value="response_2" id="response_2"/>
-        </div>
-        <div class="line">
-            <label for="response_3">René Descartes</label>
-            <input type="radio" class="radio" name="response" value="response_3"  id="response_3"/>
-        </div>
-        <div class="line">
-            <label for="response_4">Séraphin Lampion</label>
-            <input type="radio" class="radio"  name="response"value="response_4" id="response_4"/>
         </div>
 
         <input type="button" id="button" value="Envoyer"/>
